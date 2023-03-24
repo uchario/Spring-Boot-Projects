fac3fb7080f8aaaca7438835252cb879ddca665c
uchario <uchario@yahoo.com>
2023-03-24 18:53:11 +0100

    add 08-bean-lifecycle methods

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,22 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    @PostConstruct
+    public void doMyStartupStuff() {
+       System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15mins!!!";
+    }
+}
