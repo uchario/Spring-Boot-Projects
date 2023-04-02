91ca189c83c1664cc9b80d1004d1054bb5bb7823
uchario <uchario@yahoo.com>
2023-04-02 21:49:32 +0100

    finish up 04-spring-boot-rest-crud

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,2 +1,40 @@
-package com.luv2code.demo.rest;public class StudentRestController {
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<Student>();
+
+        theStudents.add(new Student("Uche", "Ariolu"));
+        theStudents.add(new Student("Sun", "Tzu"));
+        theStudents.add(new Student("Lord", "Bolton"));
+        theStudents.add(new Student("Mikel", "Gerard"));
+        theStudents.add(new Student("John", "Abruzzi"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        if((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id = " + studentId +  " not found");
+        }
+        return theStudents.get(studentId);
+    }
 }
