4b687f148d8720ec21ca4b86cd2368a98c930802
uchario <uchario@yahoo.com>
2023-04-02 21:48:07 +0100

    add Student.java

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/entity/Student.java
@@ -0,0 +1,31 @@
+package com.luv2code.demo.entity;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,2 @@
+package com.luv2code.demo.rest;public class StudentRestExceptionHandler {
+}
