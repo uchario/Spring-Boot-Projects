c89cce903b1678de5e3bdc2ab41718c26e988999
uchario <uchario@yahoo.com>
2023-06-20 05:48:08 +0100

    exploring internalization - i18n

--- in28minutes/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/hello/HelloWorldController.java
+++ in28minutes/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/hello/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.hello;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
@@ -18,5 +27,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public String helloWorldPathVariable(@PathVariable String name) {
         return String.format("Hello world! %s", name);
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
